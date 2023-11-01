4a1fa4b54384b80add27e23ed4dfbc2b7ac69fb1
Nikita Korshun <nikitakorshun@mail.ru>
2023-11-01 10:41:49 +0300

    update lab 5 bd

--- BD/Lab5/Lab5.sql
+++ BD/Lab5/Lab5.sql
@@ -1,123 +1,95 @@
--- 1. Определите общий размер области SGA.
-select sum(value) from v$sga;
+--ex1. Определите общий размер области SGA.
+    select * from V$SGA;
+    select SUM(value) from v$sga;
 
--- 2. Определите текущие размеры основных пулов SGA.
-select * from v$sga;
-select sum(min_size), sum(max_size), sum(current_size) from v$sga_dynamic_components;
+--ex2. Определите текущие размеры основных пулов SGA.
+    select * from v$sga_dynamic_components where current_size > 0;
 
--- 3. Определите размеры гранулы для каждого пула.
-select
-    component,
-    current_size,
-    max_size,
-    last_oper_mode,
-    last_oper_time,
-    granule_size,
-    current_size/granule_size as Ratio
-from v$sga_dynamic_components;
+--ex3. Определите размеры гранулы для каждого пула.
+    select component, granule_size from v$sga_dynamic_components where current_size > 0;
 
--- 4. Определите объем доступной свободной памяти в SGA.
-select current_size from v$sga_dynamic_free_memory;
+--ex4. Определите объем доступной свободной памяти в SGA.
+    select current_size from v$sga_dynamic_free_memory;
 
--- 5. Определите максимальный и целевой размер области SGA.
-select value from v$parameter where name = 'sga_target';
-select value from v$parameter where name = 'sga_max_size';
-show parameter sga; -- sqlplus
+--ex5. Определите максимальный и целевой размер области SGA.
+    SELECT value FROM v$parameter WHERE name = 'sga_target';
+    SELECT value FROM v$parameter WHERE name = 'sga_max_size';
 
--- Либо --
-select name, value
-from v$parameter
-where name like 'sga%'
-   or name = 'memory_target';
+--ex6. Определите размеры пулов КЕЕP, DEFAULT и RECYCLE буферного кэша.
+    select component, current_size, min_size, MAX_SIZE from v$sga_dynamic_components
+    where component='KEEP buffer cache' or component='DEFAULT buffer cache' or component='RECYCLE buffer cache';
 
--- 6. Определите размеры пулов КЕЕP, DEFAULT и RECYCLE буферного кэша.
-select 
-    component, 
-    min_size,
-    max_size, 
-    current_size 
-from v$sga_dynamic_components
-where component in ('KEEP buffer cache', 'DEFAULT buffer cache', 'RECYCLE buffer cache');
-
--- 7. Создайте таблицу, которая будет помещаться в пул КЕЕP.
+--ex7. Создайте таблицу, которая будет помещаться в пул КЕЕP.
 -- Продемонстрируйте сегмент таблицы.
-create table KEEP_TABLE (num int) storage (buffer_pool keep) tablespace users;
-insert into KEEP_TABLE values (1);
-insert into KEEP_TABLE values (25);
+    CREATE TABLE TableLab5 (
+        id NUMBER,
+        name VARCHAR2(50)
+    ) storage (buffer_pool keep) tablespace users;
 
-select * from KEEP_TABLE;
-select segment_name, segment_type, tablespace_name, buffer_pool
-from user_segments where segment_name like 'KEEP%';
+    SELECT segment_name, segment_type, tablespace_name, buffer_pool
+    FROM user_segments
+    WHERE segment_name = 'TABLELAB5';
 
--- 8. Создайте таблицу, которая будет кэшироваться в пуле DEFAULT. 
+    drop table TableLab5;
+
+--ex8. Создайте таблицу, которая будет кэшироваться в пуле DEFAULT.
 -- Продемонстрируйте сегмент таблицы.
-create table DEFAULT_TABLE (num int) storage (buffer_pool default) tablespace users;
-insert into DEFAULT_TABLE values (2);
-insert into DEFAULT_TABLE values (10);
+    CREATE TABLE cachedTableLab5 (
+      id NUMBER,
+      name VARCHAR2(50),
+      age NUMBER
+    ) CACHE;
 
-select * from DEFAULT_TABLE;
-select segment_name, segment_type, tablespace_name, buffer_pool
-from user_segments where segment_name like 'DEFAULT%';
+    SELECT segment_name, segment_type, tablespace_name, buffer_pool
+    FROM user_segments
+    WHERE segment_name = 'CACHEDTABLELAB5';
 
-drop table KEEP_TABLE;
-drop table DEFAULT_TABLE;
+drop table cachedTableLab5;
 
--- 9. Найдите размер буфера журналов повтора.
-show parameter log_buffer; -- sqlplus
+--ex9. Найдите размер буфера журналов повтора.
+    --SQL PLUS:
+    --show parameter log_buffer;
 
--- 10. Найдите размер свободной памяти в большом пуле.
-select * from  v$sgastat where pool = 'large pool';
+--ex10. Найдите размер свободной памяти в большом пуле.
+    SELECT POOL, bytes/1024/1024 AS "Free Memory (MB)"
+    FROM V$SGASTAT
+    WHERE POOL = 'large pool' AND name = 'free memory';
 
-select 
-    component, 
-    min_size,
-    max_size, 
-    current_size,
-    max_size - current_size as free_space
-from v$sga_dynamic_components
-where component = 'large pool';
+--ex11. Определите режимы текущих соединений с инстансом
+-- (dedicated, shared).
+    select username, service_name, server, osuser, machine, program, STATE
+    from v$session
+    where username is not null;
 
--- 11. Определите режимы текущих соединений с инстансом (dedicated, shared).
-select 
-    username, 
-    service_name, 
-    server 
-from v$session where username is not null;
+--ex12. Получите полный список работающих в настоящее время
+-- фоновых процессов.
+    SELECT name, description FROM v$bgprocess;
 
--- Либо --
-select sid, serial#, username, server, program
-from v$session
-where type != 'BACKGROUND';
+--ex13. Получите список работающих в настоящее время серверных процессов.
+    select * from v$process where addr != '00';
 
--- 12. Получите полный список работающих в настоящее время фоновых процессов.
-select count(*) from v$bgprocess;
-select name, description from v$bgprocess where paddr != hextoraw('00') order by name;
+--ex14. Определите, сколько процессов DBWn работает в настоящий момент.
+    select count(*) from v$process where addr!= '00' and pname like 'DBW%';
 
--- 13. Получите список работающих в настоящее время серверных процессов.
-select pname, program from v$process where background is null order by pname;
+--ex15. Определите сервисы (точки подключения экземпляра).
+    select * from v$active_services;
+    --select * from v$services;
 
--- 14. Определите, сколько процессов DBWn работает в настоящий момент.
-select count(*) from v$bgprocess where name like 'DBW%';
-select * from v$bgprocess where name like 'DBW%';
+--ex16. Получите известные вам параметры диспетчеров.
+    select * from v$dispatcher;
 
--- 15. Определите сервисы (точки подключения экземпляра).
-select name, network_name, pdb from v$services;
+--ex17. Укажите в списке Windows-сервисов сервис,
+-- реализующий процесс LISTENER.
+    select * from v$services;
+    --services.msc->%listener%
 
--- 16. Получите известные вам параметры диспетчеров.
-select * from v$dispatcher;
-show parameter dispatcher; -- sqlplus
+--ex18. Продемонстрируйте и поясните содержимое файла LISTENER.ORA.
+    --path: WINDOWS.x64...(this is db_home)/network/admin/listener.ora
+    --Screen: Screen_1.png
 
--- 17. Укажите в списке Windows-сервисов сервис, реализующий процесс LISTENER.
--- OracleOraDB19Home1TNSListener
-select * from v$services;
--- services.msc->%listener%
-
--- 18. Продемонстрируйте и поясните содержимое файла LISTENER.ORA.
--- Находится по пути C:\WINDOWS.X64_193000_db_home\network\admin\listener.ora
-
--- 19. Запустите утилиту lsnrctl и поясните ее основные команды.
--- В CMD пишем lsnrctl и поясняем))))
---commands:
+--ex19. Запустите утилиту lsnrctl и поясните ее основные команды.
+    --Screen: screen_2
+    --commands:
     /*
         1. start - Запускает слушатель баз данных Oracle.
         2. servacls - Отображает список сервисов и их доступа для подключений через слушателя.
@@ -132,5 +104,6 @@ select * from v$services;
         11. save_config - Сохраняет текущую конфигурацию слушателя в файл.
     */
 
--- 20. Получите список служб инстанса, обслуживаемых процессом LISTENER.
--- В CMD пишем lsnrctl и пишем services
+--ex20. Получите список служб инстанса, обслуживаемых процессом LISTENER.
+    --Screen: Screen_3
+    --command: services
